cf4514f28f2837125f1128afc7e92fc1258bbf6c
Shanawaj hossain saju <96199752+saju45@users.noreply.github.com>
2025-01-22 10:27:57 -0800

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 //cors middleware
 app.use(
   cors({
-    origin: "https://shanawaj-ecommerce.netlify.app/",
+    origin: "https://ecommerce-app-frontend-mqhg.onrender.com",
     credentials: true,
   })
 );
